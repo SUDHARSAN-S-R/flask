da18bf4d51ef37932be79bd43e0d8c9942e56eec
SUDHARSAN-S-R <sudharsansellappan@gmail.com>
2025-10-06 22:36:56 +0530

    routes

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask,request
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, World!"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello, {name}!"
+
+@app.route('/square/<int:number>')
+def square(number):
+    return str(number * number)
+
+@app.route('/handle_url')
+def handle_url():
+    if 'greeting' not in request.args or 'name' not in request.args:
+        return "Error: Please provide both 'greeting' and 'name' parameters."
+    greeting = request.args['greeting'] # key access from dictionary
+    name = request.args.get('name')
+    return f"{greeting}, {name}!"
+
 
 
 if __name__ == '__main__':
